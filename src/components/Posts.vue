777a2e4487a113f161e9f7938d2b19821ffd05e9
lucas <lucaslmota0431@gmail.com>
2021-07-20 21:33:48 -0300

    Alteraçao no componente

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,9 +1,13 @@
 <template>
     <button @click="fetchPosts">Fetch Posts</button>
     <ul>
-        <li v-for="post in posts" :key="post.id">
+        <li v-for="(post, id) in posts" :key="id">
             <h2>{{post.title}}</h2>
              {{post.body}}
+        </li>
+        <li v-for="(user, id) in users" :key="id">
+            <h2 v-if="user.id == 1">{{user.name}}</h2>
+             
         </li>
     </ul>
 </template>
@@ -16,15 +20,26 @@ export default {
         data() {
             return {
                 posts: [],
-                baseURI: "https://jsonplaceholder.typicode.com/posts"
+                users:[],
+                comments:[]
         }; 
     },
     methods:{
         fetchPosts: function(){
-            axios.get(this.baseURI).then((result) => {
-                this.posts = (result.data);
-            });
-        } 
+            // axios.get(this.baseURI).then((result) => {
+            //     this.posts = (result.data);
+            // });
+
+            axios.all([
+                axios.get('https://jsonplaceholder.typicode.com/posts'),
+                axios.get('https://jsonplaceholder.typicode.com/users'),
+                axios.get('https://jsonplaceholder.typicode.com/comments')
+            ]).then(axios.spread((postRes, userRes, commentsRes)=>{
+                this.posts = postRes.data
+                this.users = userRes.data
+                this.comments = commentsRes.da
+            }))
+        }
 
     },
 };
